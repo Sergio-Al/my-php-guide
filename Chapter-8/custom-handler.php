9f4ba932cb13efcec189613a3de331aec0e1afac
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2021-12-27 14:58:08 -0400

    Error handling part I

---
+++ Chapter-8/custom-handler.php
@@ -0,0 +1,12 @@
+<?php
+
+// we define the error handler
+$errorHandler = function (int $code, string $message, string $file, int $line) {
+    echo date(DATE_W3C), " :: $message, in [$file] on line [$line] (error code $code)", PHP_EOL;
+};
+
+// we register the error handler using set_error_handler function
+set_error_handler($errorHandler, E_ALL);
+
+// we write an expression that should trigger some error
+echo $width / $height, PHP_EOL;
