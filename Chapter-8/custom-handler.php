02ed922524f02ec92264b2d99bc99342a9a3a483
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-12-28 00:32:22 -0400

    Error Handling Part III

--- Chapter-8/custom-handler.php
+++ Chapter-8/custom-handler.php
@@ -1,7 +1,12 @@
 <?php
 
 // we define the error handler
-$errorHandler = function (int $code, string $message, string $file, int $line) {
+$errorHandler = function (
+    int $code,
+    string $message,
+    string $file,
+    int $line
+) {
     echo date(DATE_W3C), " :: $message, in [$file] on line [$line] (error code $code)", PHP_EOL;
 };
 
---
+++ Chapter-8/validate-email.php
@@ -0,0 +1,46 @@
+<?php
+
+use InvalidEmail as GlobalInvalidEmail;
+
+class InvalidEmail extends Exception
+{
+    private $context = [];
+    public function setContext(array $context)
+    {
+        $this->context = $context;
+    }
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+}
+
+function validateEmail(array $input)
+{
+    if (!isset($input[1])) {
+        throw new InvalidArgumentException('No value to check.');
+    }
+
+    $testInput = $input[1];
+    if (!filter_var($testInput, FILTER_VALIDATE_EMAIL)) {
+        $error = new InvalidEmail('The email validation has failed.');
+        $error->setContext(['testValue' => $testInput]);
+        throw $error;
+    }
+}
+
+try {
+    validateEmail($argv);
+    echo 'The input value is valid email.', PHP_EOL;
+} catch (Throwable $e) {
+    echo sprintf(
+        'Caught [%s]: %s (file: %s, line: %s, context: %s)',
+        get_class($e),
+        $e->getMessage(),
+        $e->getFile(),
+        $e->getLine(),
+        $e instanceof InvalidEmail ? json_encode($e->getContext()) : 'N/A'
+    ) . PHP_EOL;
+}
+
+// This will print an custom error message if the things goes wrong! Caught ...
