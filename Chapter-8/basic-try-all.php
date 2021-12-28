f82cb7362d35dd40a8f4361ce5cf4e40cc548941
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2021-12-27 23:12:06 -0400

    handling errors part II

---
+++ Chapter-8/basic-try-all.php
@@ -0,0 +1,19 @@
+<?php
+echo 'STRICT START' . PHP_EOL;
+try {
+    echo 'Run try block. ', PHP_EOL;
+    if (!isset($argv[1])) {
+        echo 'NO ARGUMENT: will throw exception.', PHP_EOL;
+        throw new LogicException('Argument #1 is required');
+    }
+
+    echo 'ARGUMENT: ', $argv[1], PHP_EOL;
+    var_dump(new $argv[1]);
+} catch (Error $e) {
+    echo 'ERROR: ', sprintf('%s in %s at line %d', $e->getMessage(), $e->getFile(), $e->getLine()), PHP_EOL;
+} finally {
+    echo "FINALLY blocks get executed.\n";
+}
+
+echo "Outside TRY-CATCH.\n";
+echo 'SCRIPT END.', PHP_EOL;
